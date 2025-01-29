c24548b23a9661041d10a10af9eab9a46ff1ca60
Vaibhav Singh <79097275+vaibhavsingh31390@users.noreply.github.com>
2025-01-29 17:52:52 +0530

    Add files via upload
    [INIT]

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-slate-700 p-4 text-blue-800">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ut.
+    </h1>
+    </>
+  )
+}
+
+export default App
