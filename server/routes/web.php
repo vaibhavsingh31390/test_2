24a04caaf81acb69aea95199d7cd651ccbfe833d
Vaibhav Singh <79097275+vaibhavsingh31390@users.noreply.github.com>
2025-02-01 20:37:43 +0530

    Delete server directory

--- server/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Utils\ResponseClass;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return (new ResponseClass('Application is running.', 200))->sendJsonResponse();
-});
